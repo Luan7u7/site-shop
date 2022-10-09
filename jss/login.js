518d0105b123850143c1f4eb02dd7b51ce8bc7dc
Luan7u7 <luanhenriquevg@gmail.com>
2022-10-08 23:36:13 -0300

    layout tela de login
    a tela de login está praticamente finalizada nesse ponto e comecei a parte do JS

--- jss/login.js
+++ jss/login.js
@@ -1,17 +1,17 @@
 const email = document.querySelector('#email')
 const senha = document.querySelector('#senha')
 const enviar = document.querySelector('#enviar')
+const body = document.querySelector('body')
 
-enviar.addEventListener('click', ()=>{
+enviar.addEventListener('click', () =>{
     
-    const body = document.querySelector('body')
 
     if(email.value.length == 0 && senha.value.length == 0){
 
-        console.log('olha no html pra ver se deu')
+        console.log('olha no html pra ver se deu');
 
-        div.innerHTML = 'deu'
-
-        body.append(div)
+        
     }
 })
+
+
