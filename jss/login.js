d4f7e0d9524fd79e647fb02c825faa008ee8d944
Luan7u7 <luanhenriquevg@gmail.com>
2022-10-09 22:12:48 -0300

    add de page principal

--- jss/login.js
+++ jss/login.js
@@ -2,14 +2,16 @@ const email = document.querySelector('#email')
 const senha = document.querySelector('#senha')
 const enviar = document.querySelector('#enviar')
 const body = document.querySelector('body')
+const modalContainer = document.querySelector('.modal-container')
+const modal = document.querySelector('.modal')
+const x = document.querySelector('.button')
 
-enviar.addEventListener('click', () =>{
+enviar.addEventListener('click', ({ target }) =>{
     
 
     if(email.value.length == 0 && senha.value.length == 0){
 
-        console.log('olha no html pra ver se deu');
-
+        
         
     }
 })
