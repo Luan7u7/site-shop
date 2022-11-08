49f413a79a6836626f5c3e7dd358af6c5dc16741
Luan7u7 <luanhenriquevg@gmail.com>
2022-11-08 17:50:23 -0300

    repaginação do layout index

--- js/main.js
+++ js/main.js
@@ -1,71 +1,86 @@
+
+
+//importações de referencias module
 import { menu, favorite, main, person, moreoptions, closemenu, contentSearch, inputSearch,  searchIcon, buy, favPage } from "../js/module/refs.js"
 
+// importação de funções
+import { menuclickFunc, favoriteFunc, closemenuFunc } from "./module/functions.js";
 
-import { menuclick } from "./module/menuclick.js";
-
-
+// função para atribuir a tag e class
 const createElement = (tag, className) =>{
     const element = document.createElement(tag);
     element.className = className;
     return element;
 }
 
-const firstSection = () => {
+
+const container = createElement('div', 'container');
+const section = createElement('div', 'section');
+const section1 = createElement('section', 'anuncio');
+const text = createElement('span','')
+
+const firstSection = () => { 
     
-    const container = createElement('div', 'container')
-    const section = createElement('div', 'section');
-    const legend = createElement('legend', 'legend')
+    const images = {
+        foto: 'url(https://img.ltwebstatic.com/images3_pi/2022/09/23/1663897087abeced1089303adc8c2e94fe684a4632_thumbnail_405x552.webp)',
+        foto1: 'url(https://img.ltwebstatic.com/images3_pi/2022/08/31/1661875250461190540124b4f44c2da8112f5d8073_thumbnail_405x552.webp)',
+        foto2: 'url(https://img.ltwebstatic.com/images3_pi/2020/12/09/1607481832386c7359a92c5fa7d801ccab8f31144e_thumbnail_405x552.webp)',
+        foto3: 'url(https://img.ltwebstatic.com/images3_pi/2021/06/17/162389276905c8ad5db571ea8a2be428e248f345a9_thumbnail_405x552.webp)',
+        foto4: 'url(https://img.ltwebstatic.com/images3_pi/2022/03/23/1648037329ef61759933a2e45c802c504040f0dd51_thumbnail_405x552.webp)',
+        foto5: 'url(https://img.ltwebstatic.com/images3_pi/2021/12/27/16405850862c25c272a56425e1d7cb0beef6f6c925_thumbnail_405x552.webp)',
+    }
+
+    const div = createElement('div', '')
+    const div1 = createElement('div', '');
+    const div2 = createElement('div', '');
+    const div3 = createElement('div', '');
+    const div4 = createElement('div', '');
+    const div5 = createElement('div', ''); 
     
-    const div1 = createElement('div', 'white');
-    const white = createElement('span', 'white')
-    div1.style.backgroundImage = `url(https://imgnike-a.akamaihd.net/1300x1300/01675551A4.jpg)`
-    legend.innerHTML = 'R$297,67'
-    
-    const div2 = createElement('div', 'f1');
-    const midse = createElement('span', 'midse')
-    div2.style.backgroundImage = `url('https://imgnike-a.akamaihd.net/1300x1300/014626P1A4.jpg')`
-    
-    const div3 = createElement('div', 'f1');
-    const xux = createElement('span', 'xux')
-    div3.style.backgroundImage = `url('https://imgnike-a.akamaihd.net/1300x1300/0114147UA4.jpg')`
+    div.style.backgroundImage = images.foto
+    div1.style.backgroundImage = images.foto1
+    div2.style.backgroundImage = images.foto2
+    div3.style.backgroundImage = images.foto3
+    div4.style.backgroundImage = images.foto4
+    div5.style.backgroundImage = images.foto5
 
 
-    const div4 = createElement('div', 'f1');
-    const zion2 = createElement('span', 'zion2')
-    div4.style.backgroundImage = `url('https://imgnike-a.akamaihd.net/900x900/02235051A4.jpg')`
-    div4.style.backgroundSize = 'contain'
-   
-    
-    const div5 = createElement('div', 'f1');
-    const orange = createElement('span', 'orange')
-    div5.style.backgroundImage = `url('https://imgnike-a.akamaihd.net/1300x1300/01648755A4.jpg')`
-    
-    const div6 = createElement('div', 'f1');
-    const xxxvilow = createElement('span', 'xxxvilow')    
-    div6.style.backgroundImage = `url('https://imgnike-a.akamaihd.net/900x900/013631IDA4.jpg')`
+    ext.innerHTML = 'R$69,90'
     
 
-    main.appendChild(container)
-    container.appendChild(section);
-    section.appendChild(div1);
-    section.appendChild(div2);
-    section.appendChild(div3);
-    section.appendChild(div4);
-    section.appendChild(div5);
-    section.appendChild(div6);
-    div1.appendChild(white);
-    div2.appendChild(midse);
-    div3.appendChild(xux);
-    div4.appendChild(zion2);
-    div5.appendChild(orange);
-    div6.appendChild(xxxvilow);
+    // main.appendChild(container)
+    main.appendChild(section)
+    section.appendChild(div)
+    div.appendChild(text)
+    section.appendChild(div1)
+    section.appendChild(div2)
+    section.appendChild(div3)
+    section.appendChild(div4)
+    section.appendChild(div5)
+    
+    return container
+}
 
+const createAnuncio = () => {
+
+    const div = createElement('div', 'imagem')
+    const text = createElement('span', 'texto')
+
+    text.innerHTML = 'O conforto de milhões <br> <sup>novo air-drop p<small>99</small></sup><br><br><br> R$978,97'
+    div.style.backgroundImage = `url('https://imgnike-a.akamaihd.net/900x900/0119897U.jpg')`
+    div.style.backgroundSize = 'contain'
+    div.style.backgroundPosition = 'bottom right'
+
+    main.append(container)
+    container.appendChild(section1)
+    section1.appendChild(div)
+    div.appendChild(text)
     return container;
 }
 
 const secondSection = () => {
-    const container = createElement('div', 'container')
-    const section = createElement('div', 'section');
+    // const container = createElement('div', 'container')
+    const section2 = createElement('div', 'section2');
     const div = createElement('div', 'f1');
     div.style.backgroundImage = `url('https://imgnike-a.akamaihd.net/900x900/0229177T.jpg')`
     
@@ -86,14 +101,15 @@ const secondSection = () => {
     div6.style.backgroundImage = `url('https://imgnike-a.akamaihd.net/900x900/021895ID.jpg')`
 
 
-    main.appendChild(container)
-    container.appendChild(section);
-    section.appendChild(div);
-    section.appendChild(div2);
-    section.appendChild(div3);
-    section.appendChild(div4);
-    section.appendChild(div5);
-    section.appendChild(div6);
+    // main.appendChild(container)
+    // container.appendChild(section2);
+    main.appendChild(section2);
+    section2.appendChild(div);
+    section2.appendChild(div2);
+    section2.appendChild(div3);
+    section2.appendChild(div4);
+    section2.appendChild(div5);
+    section2.appendChild(div6);
 
     return container;
 }
@@ -239,29 +255,6 @@ const createContainerXXXVILow = () => {
     return container;
 }
 
-const createContainerAnuncio = () => {
-
-    const container = createElement('div', 'container')
-    const content = createElement('section', 'anuncio')
-    const div = createElement('div', 'imagem')
-    const text = createElement('span', 'texto')
-
-    text.innerHTML = 'O conforto de milhões <br> <sup>novo air-drop p<small>99</small></sup><br><br><br> R$978,97'
-    div.style.backgroundImage = `url('https://imgnike-a.akamaihd.net/900x900/0119897U.jpg')`
-    // div.style.scale = '1.3'
-    div.style.backgroundSize = 'contain'
-    div.style.backgroundPosition = 'bottom right'
-
-
-
-    main.appendChild(container);
-    container.appendChild(content);
-    content.appendChild(div);
-    div.appendChild(text)
-
-    return container;
-}
-
 const createContainerAnuncio2 = () => {
 
     const container = createElement('div', 'container')
@@ -287,63 +280,53 @@ const createContainerAnuncio2 = () => {
     return container;
 }
 
-createContainerAnuncio();
+
+
+
+createAnuncio();
 firstSection();
 secondSection();
-createContainerAnuncio2();
-thirdSection();
-fourthSection();
-fifthSection();
-createContainerXXXVILow();
-
-menu.addEventListener('click', menuclick)
+// createContainerAnuncio2();
+// thirdSection();
+// fourthSection();
+// fifthSection();
+// createContainerXXXVILow();
 
 
-closemenu.addEventListener('click', () => {
+//click de abri a aba de menu
+menu.addEventListener('click', menuclickFunc)
 
-    if(moreoptions.style.transform = 'translate(-340px, 0px)'){
 
-        moreoptions.style.visibility = 'hidden'
-        moreoptions.style.transform = 'translate(-300px, 0px)'
-        main.style.transform = 'translate(0px, 0px)'
-    }
-    
-    //tirar o o clipping azul na hora de fechar o menu
-})
+//click de fechar a aba menu
+closemenu.addEventListener('click', closemenuFunc)
+
+//tirar o o clipping azul na hora de fechar o menu
+
 
 
 searchIcon.addEventListener('click', (event) => {
-
+    
     console.log('foi');
-
+    
     if (inputSearch.style.display == 'none'){
-
+        
         inputSearch.style.display = 'flex'
     }
-   
-})
-
-
-favorite.addEventListener('click', () => {
-
-
-    if(favPage.style.transform == 'translate(0px, 0px)'){
-
-        favPage.style.transform = 'translate(0px, 800px)'
-        favPage.style.visibility = 'visible'
-    }
-    else {
-        
-        favPage.style.visibility = 'visible'
-        favPage.style.transform = 'translate(0px, 0px)'
-    }
-})
-
     
+})
+
+
+//click de abrir e fechar a aba de favoritos
+favorite.addEventListener('click', favoriteFunc)
+    
+
+
 person.addEventListener('click', () => {
 
     window.location = './perfil.html'
 })
+
+
 
 buy.addEventListener('click', () => {
 
---
+++ js/module/functions.js
@@ -0,0 +1,42 @@
+import { menu, favorite, main, person, moreoptions, closemenu, contentSearch, inputSearch,  searchIcon, buy, favPage } from "./refs.js"
+
+
+export function menuclickFunc(){
+
+    if(moreoptions.style.transform = 'translate(-340px, 0px)'){
+        moreoptions.style.transform = 'translate(0px, 0px)'
+        moreoptions.style.transition = 'all .4s'
+        main.style.transform = 'translate(0px, 85px)'
+        moreoptions.style.visibility = 'visible'
+    }
+    else {
+        moreoptions.style.transform = 'translate(-340px, 0px)'
+        moreoptions.style.visibility = 'hidden' 
+    }
+}
+
+
+export function favoriteFunc(){
+ 
+    
+    if(favPage.style.transform == 'translate(0px, 0px)'){
+
+        favPage.style.transform = 'translate(0px, 800px)'
+        favPage.style.visibility = 'hidden'
+    }
+    else {
+        
+        favPage.style.visibility = 'visible'
+        favPage.style.transform = 'translate(0px, 0px)'
+    }
+}
+
+export function closemenuFunc(){
+
+    if(moreoptions.style.transform = 'translate(-340px, 0px)'){
+
+        moreoptions.style.visibility = 'hidden'
+        moreoptions.style.transform = 'translate(-300px, 0px)'
+        main.style.transform = 'translate(0px, 0px)'
+    }
+}
